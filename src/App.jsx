50bc59aaea3319e0722c39500220992f2d19bd02
Rachit Srivastava <rachitsrivastava76@gmail.com>
2026-02-03 04:57:08 +0530

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ export default function App() {
           margin: 0;
           padding: 0;
           background: #0b0d10;
-          cursor: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 397.7 311.7"><defs><linearGradient id="a" x1="360.88" y1="351.46" x2="141.21" y2="-69.29" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="%2300ffa3"/><stop offset="1" stop-color="%23dc1fff"/></linearGradient><linearGradient id="b" x1="264.83" y1="401.6" x2="45.16" y2="-19.15" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="%2300ffa3"/><stop offset="1" stop-color="%23dc1fff"/></linearGradient></defs><path d="M64.6 237.9c2.4-2.4 5.7-3.8 9.2-3.8h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1z" fill="url(%23a)"/><path d="M64.6 3.8C67.1 1.4 70.4 0 73.8 0h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1z" fill="url(%23b)"/><path d="M333.1 120.1c-2.4-2.4-5.7-3.8-9.2-3.8H6.5c-5.8 0-8.7 7-4.6 11.1l62.7 62.7c2.4 2.4 5.7 3.8 9.2 3.8h317.4c5.8 0 8.7-7 4.6-11.1z" fill="url(%23a)"/></svg>'), auto;
         }
         * {
           box-sizing: border-box;
